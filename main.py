0486bca0eeb5aaba5c819e746a40513a6858093e
Programmēšana VVS <>
2021-05-05 08:25:38 +0000

    Sākuma izvēlnes definēšana

---
+++ main.py
@@ -0,0 +1,12 @@
+import kontakti #tiek importēts fails "kontakti.py"
+from os import system
+sakums = """ Sveicināti!
+------------------------------
+Lūdzu, izvēlieties darbību:
+1 - pievienot jaunu kontaktu
+2 - atrast kontaktu
+3 - rediģēt kontaktu
+4 - kontaktu dzēšana
+------------------------------
+"""
+
